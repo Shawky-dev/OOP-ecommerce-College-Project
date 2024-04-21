7d02a45c8a390d802701970caa4c6c94acf245cb
ok1484478@gmail.com <ok1484478@gmail.com>
2024-04-22 01:34:44 +0200

    Added user class added inherited customer class

---
+++ backend/server/server/customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server
+{
+    internal class customer:users
+    {
+        
+        private string address;
+        public void setUserAddress(string add)
+        {
+            address = add;
+
+
+        }
+    }
+}
