0ac5588479e5cf0c078f43dbb60aa90814ddb32e
Omar Abdel Nasser <165521461+AmoryCR@users.noreply.github.com>
2024-04-22 23:27:37 +0200

    Add files via upload

---
+++ Buyer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class Buyer : Informations
+{
+    public Buyer(string fn, string ln, string e, string p, string ph, string c)
+    : base(fn, ln, e, p, ph, c)
+    { }
+    static void Main(string[] args)
+    {
+        string filepath = @"C:\Users\omar\source\repos\OOP_Project\OOP_Project\BuyersList.txt";
+        List<string> lines = new List<string>();
+        List<string> BuyersPhone = new List<string>();
+        List<string> BuyersEmail = new List<string>();
+        List<string> BuyersPassword = new List<string>();
+        List<Buyer> Buyers = new List<Buyer>();
+
+        lines = File.ReadAllLines(filepath).ToList();
+        foreach (string line in lines)
+        {
+            string[] b = line.Split(',');
+            Buyer B = new Buyer(b[0], b[1], b[2], b[3], b[4], b[5]);
+            BuyersEmail.Add(b[2]);
+            BuyersPassword.Add(b[3]);
+            BuyersPhone.Add(b[4]);
+            Buyers.Add(B);
+        }
+        foreach (Buyer B in Buyers)
+        {
+            Console.WriteLine(B);
+        }
+    }
+    public override string Updatephone( List <string> BuyersPhone, string np)
+    {
+        return base.Updatephone(BuyersPhone,np);
+    }
+    public override string UpdateEmail(List<string> BuyersEmail, string np)
+    {
+        return base.Updatephone(BuyersEmail, np);
+    }
+    public override string Updatepassword(List<string> BuyersPassword, string np)
+    {
+        return base.Updatephone(BuyersPassword, np);
+    }
+    public override string UpdateCity(Buyer B,string c)
+    {
+        return base.UpdateCity(B,c);
+    }
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+}
---
+++ Informations.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+    public class Informations
+    {
+    private string email ;
+    private string password ;
+    public string FirstName
+    { get; set; }
+    public string LastName
+    { get; set; }
+    public string phone
+    { get; set; }
+    public string city
+    { get; set; }
+    public Informations(string fn, string ln, string e, string p, string ph , string c)
+    {
+        FirstName = fn;
+        LastName = ln;
+        Email = e;
+        Password = p;
+        phone = ph;
+        city = c ;
+    }
+    public string Email
+    {
+        get { return email; }
+        set
+        {
+        email = value;
+        }
+    }
+    public string Password
+    {
+        get { return password ; }
+        set
+        {
+        password = value ;
+        }
+    }
+    public virtual string Updatephone(List<string> BuyersPhone , string np)
+    {
+        int n = 0;
+        while(n!=1)
+        {
+            for(int i = 0 ; i < BuyersPhone.Count; i++)
+            {
+                if(BuyersPhone[i] != np)
+                {
+                    n = 1 ;
+                }
+                 else
+                { n = 0 ; }
+            }
+            if(n==0)
+            {
+                // when the buyer enter phone used ,should enter another one
+            }
+        }
+        return np ;
+    }
+    public virtual string UpdateEmail(List<string> BuyersEmail , string ne)
+    {
+        int k = 0;
+        while (k != 1)
+        {
+            for (int i = 0; i < BuyersEmail.Count; i++)
+            {
+                if (BuyersEmail[i] != ne)
+                {
+                    k = 1;
+                }
+                else
+                { k = 0; }
+            }
+            if (k == 0)
+            {
+                // when the buyer enter email used ,should enter another one
+            }
+        }
+        return ne;
+    }
+    public virtual string Updatepassword(List<string> BuyersPassword, string npw)
+    {
+        int l = 0;
+        while (l != 1)
+        {
+            for (int i = 0; i < BuyersPassword.Count; i++)
+            {
+                if (BuyersPassword[i] != npw)
+                {
+                    l = 1;
+                }
+                else
+                { l = 0; }
+            }
+            if (l == 0)
+            {
+                // when the buyer enter password used ,should enter another one
+            }
+        }
+        return npw;
+    }
+    public virtual string UpdateCity(Buyer B , string c)
+    {
+        B.city = c ;
+        return B.city ;
+    }
+    public override string ToString()
+    {
+        return $"{FirstName}\t{LastName}\t{Email}\t{Password}\t{phone}\t{city}" ;
+    }
+}
